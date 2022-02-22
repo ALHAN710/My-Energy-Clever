c05fb8c5d3f39b738fdaa16c266b0d163c4834cf
PASCAL ALHADOUM <alhadoumpascal@gmail.com>
2022-02-22 23:51:45 +0100

    Modification de la date à celle envoyé par le module

--- src/Controller/GensetController.php
+++ src/Controller/GensetController.php
@@ -655,8 +655,8 @@ class GensetController extends ApplicationController
             //$date = new DateTime($paramJSON['date']);
             if (array_key_exists("date", $paramJSON)) {
                 //Récupération de la date dans la requête et transformation en object de type Date au format date SQL
-                //$date = DateTime::createFromFormat('Y-m-d H:i:s', $paramJSON['date']);
-                $date = new DateTime('now');
+                $date = DateTime::createFromFormat('Y-m-d H:i:s', $paramJSON['date']);
+                //$date = new DateTime('now');
                 // //dump($date);
                 //die();
 
