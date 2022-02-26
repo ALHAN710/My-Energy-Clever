d188e2c24b1573884c404435d920f9bbdcc0a1bb
PASCAL ALHADOUM <alhadoumpascal@gmail.com>
2022-02-26 07:45:14 +0100

    Optimisation du calcul de l'énergie produite par le Groupe électrogène

--- src/Service/GensetModService.php
+++ src/Service/GensetModService.php
@@ -976,6 +976,25 @@ class GensetModService
             ->getResult();
         $totalTEP = $TEPdata[0]['TEP'] ?? 0;
 
+        if ($totalTEP === 0) {
+            $TEPdata = $this->manager->createQuery("SELECT d.dateTime as dat, SUM(d.eaa + d.eab + d.eac) AS TEP
+                                        FROM App\Entity\GensetData d
+                                        JOIN d.smartMod sm 
+                                        WHERE d.dateTime BETWEEN :startDate AND :endDate
+                                        AND sm.id = :smartModId         
+                                        ")
+                ->setParameters(array(
+                    'startDate'    => $this->startDate->format('Y-m-d H:i:s'),
+                    'endDate'      => $this->endDate->format('Y-m-d H:i:s'),
+                    'smartModId'   => $this->gensetMod->getId()
+                ))
+                ->getResult();
+
+            $totalTEP = $TEPdata[0]['TEP'] ?? 0;
+        }
+
+        $totalTEP = floatval(number_format((float) $totalTEP, 2, '.', ''));
+
         $TEPdata = $this->manager->createQuery("SELECT SUBSTRING(d.dateTime,1,10) as dat, MAX(d.totalEnergy) - MIN(NULLIF(d.totalEnergy,0)) AS TEP
                                         FROM App\Entity\GensetData d
                                         JOIN d.smartMod sm 
