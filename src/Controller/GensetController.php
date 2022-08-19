3c4220a996f98f69892829bae369dc4ea958bc9c
ALHAN710 <alhadoumpascal@gmail.com>
2022-08-19 15:01:01 +0100

    Gestion d'envoi des notifications aux contacts de site uniquement en production

--- src/Controller/GensetController.php
+++ src/Controller/GensetController.php
@@ -1317,6 +1317,7 @@ class GensetController extends ApplicationController
                     ->setAlarm($alarmCode)
                     ->setCreatedAt($date);
 
+                $site = null;
 
                 if ($smartMod->getSite()) $site = $smartMod->getSite();
                 else {
@@ -1346,10 +1347,12 @@ class GensetController extends ApplicationController
                     } else $message = $alarmCode->getLabel() . ' du site ' . $site->getName() . ' survenu(e) le ' . $date->format('d/m/Y à H:i:s');
                 }
 
-                /*foreach ($site->getContacts() as $contact) {
-                    $messageBus->dispatch(new UserNotificationMessage($contact->getId(), $message, $alarmCode->getMedia(), $alarmCode->getAlerte()));
-                    //$messageBus->dispatch(new UserNotificationMessage($contact->getId(), $message, 'SMS', ''));
-                }*/
+                if ($this->getParameter('app.env') !== "dev") {
+                    foreach ($site->getContacts() as $contact) {
+                        $messageBus->dispatch(new UserNotificationMessage($contact->getId(), $message, $alarmCode->getMedia(), $alarmCode->getAlerte()));
+                        //$messageBus->dispatch(new UserNotificationMessage($contact->getId(), $message, 'SMS', ''));
+                    }
+                }
 
                 //$adminUsers = [];
                 $Users = $manager->getRepository('App:User')->findAll();
